0d977182c707e9262cfa195c4b416ef33b5b6b21
Your Name <you@example.com>
2015-03-20 04:24:38 -0400

    url setting

--- openapp/api/auth.py
+++ openapp/api/auth.py
@@ -3,9 +3,11 @@ from webob import exc
  
 @wsgify.middleware
 def auth_filter(request, app):
-    if request.headers.get('X-Auth-Token') != 'test-token':
+    if request.headers.get('X-Auth-Token') == 'test-token' and \
+       request.path_info == "/template":
+        return app(request) 
+    else:
         return exc.HTTPForbidden()
-    return app(request)
  
 def filter_factory(global_config, **local_config):
     return auth_filter 
