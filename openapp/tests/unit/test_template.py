ff50e987e6e3bc57737929f33bdfdbccb52e2066
Yusuke Kawamura <you@example.com>
2015-03-24 15:28:14 +0900

    update test_template
    update README

--- openapp/tests/unit/test_template.py
+++ openapp/tests/unit/test_template.py
@@ -1,13 +1,16 @@
-import openapp.api.auth as template
+import openapp.api.template.template as template
 import unittest
 import webob
 
 
+class FakeApp(object):
+
+    def __call__(self, env, start_response):
+        return Response(body = "OK")(env, start_response)
+
 class RestTest(unittest.TestCase):
-    def test_auth_filter(self):
+    def test_template_application(self):
         request = webob.Request.blank('/template')
         request.headers["X-Auth-Token"] = "test-token"
-        response = request.get_response(template.filter_factory)
-#        self.assertEqual(expected, actual)
-#if __name__ == "__main__":
-#    unittest.main()
+        response = request.get_response(template.application)
+        self.assertEqual(response.body, "Sample API Test Return OK\n")
